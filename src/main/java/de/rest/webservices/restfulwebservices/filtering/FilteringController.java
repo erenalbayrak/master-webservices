e44531bdd1b5c75261025108ea0f21e76f0e1b82
eren <erenalbayrak92@gmail.com>
2018-04-30 17:18:18 +0200

    Filtering Dynamically

--- src/main/java/de/rest/webservices/restfulwebservices/filtering/FilteringController.java
+++ src/main/java/de/rest/webservices/restfulwebservices/filtering/FilteringController.java
@@ -1,13 +1,30 @@
 package de.rest.webservices.restfulwebservices.filtering;
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FilteringController {
 
-    @GetMapping("/filtering")
-    public SomeBean retriveSomeBean() {
-        return new SomeBean("value1", "value2", "value3");
+    @GetMapping("/filtering-statically")
+    public SomeBeanForStaticFiltering retriveSomeBean() {
+        return new SomeBeanForStaticFiltering("value1", "value2", "value3");
+    }
+
+    @GetMapping("/filtering-dynamically")
+    public MappingJacksonValue retriveSomeBeanDynamically() {
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1", "field2");
+        FilterProvider filterProvider = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+
+        SomeBeanForDynamicFiltering someBean = new SomeBeanForDynamicFiltering("value1", "value2", "value3");
+        MappingJacksonValue mapping = new MappingJacksonValue(someBean);
+        mapping.setFilters(filterProvider);
+
+        return mapping;
     }
 }
---
+++ src/main/java/de/rest/webservices/restfulwebservices/filtering/SomeBeanForDynamicFiltering.java
@@ -0,0 +1,41 @@
+package de.rest.webservices.restfulwebservices.filtering;
+
+import com.fasterxml.jackson.annotation.JsonFilter;
+
+@JsonFilter("SomeBeanFilter")
+public class SomeBeanForDynamicFiltering {
+
+    private String field1;
+    private String field2;
+    private String field3;
+
+    public SomeBeanForDynamicFiltering(String value1, String value2, String value3) {
+        field1 = value1;
+        field2 = value2;
+        field3 = value3;
+    }
+
+    public String getField1() {
+        return field1;
+    }
+
+    public void setField1(String field1) {
+        this.field1 = field1;
+    }
+
+    public String getField2() {
+        return field2;
+    }
+
+    public void setField2(String field2) {
+        this.field2 = field2;
+    }
+
+    public String getField3() {
+        return field3;
+    }
+
+    public void setField3(String field3) {
+        this.field3 = field3;
+    }
+}
