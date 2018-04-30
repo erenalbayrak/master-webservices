0eacafb5b35861c4cec405eac1697ebe09965cd3
eren <erenalbayrak92@gmail.com>
2018-04-30 13:39:56 +0200

    Swagger2 Documentation

--- src/main/java/de/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/de/rest/webservices/restfulwebservices/user/User.java
@@ -1,17 +1,22 @@
 package de.rest.webservices.restfulwebservices.user;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
-
+@ApiModel(description = "All details about the user.")
 public class User {
 
     private String id;
 
     @Size(min = 2, message = "Name muss mindestends 2 Zeichen lang sein.")
+    @ApiModelProperty(notes = "Name have to contain at least 2 characters.")
     private String name;
 
     @Past
+    @ApiModelProperty(notes = "Birth date have to be in the past.")
     private Date birthDate;
 
     // Ohne Default-Konstruktor HTTP 500 Error
